4e8920a2aa93b1fce36edd06c511fc57cf17500e
N. Dechev <n.dechev@abv.bg>
2023-07-21 11:49:02 +0300

    Refactored Helpers class.
    Removed GetRestrictedCommands() method, added separate classes for Administrator and Customer commands and AddCommands() method now is InitializeCommands().

---
+++ Shopping Cart v2/ShoppingCart v2/ShoppingCart/ShoppingCart/commands/cart/AddCartItemCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.commands
+{
+
+    class AddCartItemCommand : ICommand
+    {
+        public void Execute(string[] commandParts, ProductList productList, ShoppingCart shoppingCart)
+        {
+            if (commandParts.Length == 3)
+            {
+                int productId = int.Parse(commandParts[1].Trim());
+                int quantity = int.Parse(commandParts[2].Trim());
+
+                Product product = productList.GetProductById(productId);
+
+
+                if (shoppingCart.AddCartItem(productList, productId, quantity))
+                {
+                    Console.WriteLine($"Product {product.Name} added to the cart successfully!");
+                }
+                else
+                {
+                    Console.WriteLine("Product not found or insufficient quantity available.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid command format for adding a cart item.");
+            }
+        }
+
+        public void Help()
+        {
+            Console.WriteLine("addCartItem | {productId} | {quantity}");
+            Console.WriteLine("\tAdds the specified quantity of the product with the given productId to the shopping cart.");
+        }
+    }
+}
---
+++ Shopping Cart v2/ShoppingCart v2/ShoppingCart/ShoppingCart/commands/cart/ListCartItems.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.commands.cart
+{
+    class ListCartItems : ICommand
+    {
+        public void Execute(string[] commandParts, ProductList productList, ShoppingCart shoppingCart)
+        {
+            shoppingCart.DisplayCartItems(productList);
+        }
+
+        public void Help()
+        {
+            Console.WriteLine("listCartItems");
+            Console.WriteLine("\tDisplays the items in the shopping cart.");
+        }
+    }
+
+}
