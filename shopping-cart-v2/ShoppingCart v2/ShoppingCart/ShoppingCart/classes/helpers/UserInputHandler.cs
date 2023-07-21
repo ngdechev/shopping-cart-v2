80dfec75307e4223c26930b53dbe076dbcb17a1a
N. Dechev <n.dechev@abv.bg>
2023-07-21 10:47:51 +0300

    Refactored Helpers class.
    Removed GetRestrictedCommands() method, added separate classes for Administrator and Customer commands and AddCommands() method now is InitializeCommands().

---
+++ shopping-cart-v2/ShoppingCart v2/ShoppingCart/ShoppingCart/classes/helpers/UserInputHandler.cs
@@ -0,0 +1,52 @@
+﻿using OnlineShop.commands;
+using System;
+
+public class UserInputHandler
+{
+    public static UserRole GetUserRole()
+    {
+        Console.WriteLine("Welcome to the Shopping Cart App!");
+        Console.WriteLine("Available Commands:");
+        Console.WriteLine("login - Log in with a user role");
+        Console.WriteLine("help - Show available commands");
+        Console.WriteLine("exit - Exit the application");
+
+        while (true)
+        {
+            string userInput = Console.ReadLine().ToLower();
+            if (userInput == "login")
+            {
+                // Ask the user to select a user role
+                Console.WriteLine("Please select a user role:");
+                Console.WriteLine("1. Administrator");
+                Console.WriteLine("2. Customer");
+
+                while (true)
+                {
+                    string roleChoice = Console.ReadLine();
+                    if (int.TryParse(roleChoice, out int roleNumber) && Enum.IsDefined(typeof(UserRole), roleNumber - 1))
+                    {
+                        return (UserRole)(roleNumber - 1);
+                    }
+                    Console.WriteLine("Invalid input. Please enter the number corresponding to the desired role.");
+                }
+            }
+            else if (userInput == "help")
+            {
+                Console.WriteLine("Available Commands:");
+                Console.WriteLine("1. login - Log in with a user role");
+                Console.WriteLine("2. help - Show available commands");
+                Console.WriteLine("3. exit - Exit the application");
+            }
+            else if (userInput == "exit")
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Invalid command. Type 'help' for available commands.");
+            }
+        }
+    }
+}
+
---
+++ shopping-cart-v2/ShoppingCart v2/ShoppingCart/ShoppingCart/commands/product/SearchProductsCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.commands
+{
+    class SearchProductsCommand : ICommand
+    {
+        public void Execute(string[] commandParts, ProductList productList, ShoppingCart shoppingCart)
+        {
+            if (commandParts.Length == 2)
+            {
+                string searchTerm = commandParts[1].Trim();
+                productList.SearchProducts(searchTerm);
+            }
+            else
+            {
+                Console.WriteLine("Invalid command format for searching products.");
+            }
+        }
+
+        public void Help()
+        {
+            Console.WriteLine("searchProducts | {searchTerm}");
+            Console.WriteLine("\tSearches for products matching the specified searchTerm in their name or description.");
+        }
+    }
+}
